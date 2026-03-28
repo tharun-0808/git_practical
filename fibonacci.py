2d0de3d49305f829a543216887a6a38e09e7f845
Tharun <tharun0808@gmail.com>
2026-03-28 16:49:22 +0530

    Updated fibonacci program

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,4 @@
-n = 10
+n = 12
 a, b = 0, 1
 
 print("Fibonacci Series:")
@@ -6,4 +6,5 @@ print("Fibonacci Series:")
 for i in range(n):
     print(a, end=" ")
     a, b = b, a + b
-    
+
+print("\nProgram completed successfully")
